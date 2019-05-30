0087aa5e4c2c0a21737506a780610ca80915bafc
littleknightgames <38872872+littleknightgames@users.noreply.github.com>
2019-05-30 14:54:24 -0400

    fixed bugs with list and search bar and clear

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,27 +1,47 @@
 import React from "react";
 import "./Todo.css";
 export default class List extends React.Component {
-
   clearHandler = e => {
-    if(e.target.className === "normal"){
-      e.target.className = "striked"
-      this.props.cb(e.target.id,true);
-    }else{
-      e.target.className = "normal"
-      this.props.cb(e.target.id,false);
+    console.log(e.target.getAttribute("data-searched"));
+    if (e.target.className.includes(`normal`)) {
+      if (e.target.getAttribute("data-searched") === true) {
+        e.target.className = `striked searched`;
+      }
+      if (e.target.getAttribute("data-searched") === false) {
+        e.target.className = `striked nsearched`;
+      }
+      this.props.cb(e.target.id, true);
     }
-  }
+    if (e.target.className.includes(`striked`)) {
+      if (e.target.getAttribute("data-searched") === true) {
+        e.target.className = `normal searched`;
+      }
+      if (e.target.getAttribute("data-searched") === false) {
+        e.target.className = `normal nsearched`;
+      }
+      this.props.cb(e.target.id, false);
+    }
+  };
 
   listHandler = () => {
-    if(this.props.list){
-    return this.props.list.map(item => {
-      return (
-        <p onClick={this.clearHandler} className={(item.completed === false) ? `normal ${(item.searched === true ? "searched" : "nsearched")}` : `striked ${(item.searched === true ? "searched" :"nsearched")}`} id={item.id} key={item.id}>
-          {item.task}
-        </p>
-      );
-    });
-  }
+    if (this.props.list) {
+      return this.props.list.map(item => {
+        return (
+          <p
+            onClick={this.clearHandler}
+            className={
+              item.completed === false
+                ? `normal ${item.searched === true ? "searched" : "nsearched"}`
+                : `striked ${item.searched === true ? "searched" : "nsearched"}`
+            }
+            id={item.id}
+            data-searched={item.searched}
+          >
+            {item.task}
+          </p>
+        );
+      });
+    }
   };
   render() {
     return <div>{this.listHandler()}</div>;
